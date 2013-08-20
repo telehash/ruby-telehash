cb24aec52b9256c42f476d2d1fd2ff7a6946a2cd
David Waite <david@alkaline-solutions.com>
2013-08-19 18:10:43 -0600

    Add packet encoding

--- lib/telehash/packet.rb
+++ lib/telehash/packet.rb
@@ -1,7 +1,18 @@
 require 'openssl'
+require 'json'
 
 module Telehash
   class Packet
+    attr_accessor :json, :data
     
+    def initialize json, data = nil
+      @json = json
+      @data = data
+    end
+    
+    def to_s
+      json_text = @json.to_json
+      [json_text.length, json_text, @data].pack "nA*A*"
+    end
   end
 end
--- specs/packet_spec.rb
+++ specs/packet_spec.rb
@@ -3,6 +3,26 @@ require 'minitest/spec'
 
 require 'telehash'
 
-describe Telehash::Packet do
-  it 'can be created from a switch'
+Packet = Telehash::Packet
+
+describe Packet do
+  it 'can be created from just an empty hash' do
+    p = Packet.new({})
+    p.must_be_instance_of Packet
+    p.json.must_be_instance_of Hash
+    p.to_s.must_equal "\0\2{}".encode("UTF-8")
+  end
+  it 'can be created from an empty hash with data' do
+    p = Packet.new({}, "abcd")
+    p.must_be_instance_of Packet
+    p.json.must_be_instance_of Hash
+    p.data.must_be_instance_of String
+    p.to_s.must_equal "\0\2\{}abcd".encode("UTF-8")
+  end
+  it 'can be created with non-empty json' do
+    p = Packet.new({a: 'b'})
+    p.must_be_instance_of Packet
+    p.json.must_be_instance_of Hash
+    p.to_s.must_equal "\0\t{\"a\":\"b\"}".encode("UTF-8")
+  end
 end
